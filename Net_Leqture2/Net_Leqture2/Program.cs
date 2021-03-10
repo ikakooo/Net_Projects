27c3ff8fd2fa319eecb2a7a59e496bae9462982c
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-03-11 00:02:34 +0400

    errgrggfd

--- Net_Leqture2/Net_Leqture2/Program.cs
+++ Net_Leqture2/Net_Leqture2/Program.cs
@@ -8,25 +8,54 @@ namespace Net_Leqture2
 {
     class Program
     {
+        public static void prceString()
+        {
+            int number = 0;
+            bool isParsed = false;
+            while (!isParsed)
+            {
+                String s = Console.ReadLine();
+                
+                  isParsed = int.TryParse(s, out number);
+
+                if (isParsed)
+                {
+                    Console.WriteLine("parsed: "+ number);
+                }
+                else
+                {
+                    Console.WriteLine("Can't parce,try again");
+                }
+
+               
+
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            String s = Console.ReadLine();
-            int i = int.Parse("123334");
-           
-            bool isParsed = int.TryParse(s, out i);
-            Console.WriteLine(i);
-           
 
-            if (isParsed)
-            {
-                Console.WriteLine("parsed");
-            }
-            else
-            {
-                Console.WriteLine("Can't parce");
-            }
-             Console.ReadLine();
+            //String s = Console.ReadLine();
+            //int d = int.Parse("123334");
+            //int i = 0;
 
+            //bool isParsed = int.TryParse(s, out d);
+            //Console.WriteLine(d);
+
+
+            //if (int.TryParse(s, out i))
+            //{
+            //    Console.WriteLine("parsed");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Can't parce");
+            //}
+
+            prceString();
+            Console.ReadLine();
+           
         }
     }
 }
