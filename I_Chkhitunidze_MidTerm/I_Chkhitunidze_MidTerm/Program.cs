b3b43a76d6d3323a4b1dfcd15dec3420dec22a48
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-04-15 21:05:44 +0400

    midterm

---
+++ I_Chkhitunidze_MidTerm/I_Chkhitunidze_MidTerm/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Chkhitunidze_MidTerm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            calculator();
+
+
+
+            ///Task 2
+            Student student1 = new Student();
+
+            student1.cars.Add("Mersedes");
+            student1.cars.Add("Wolwo");
+            student1.cars.Add("BMW");
+            student1.cars.Add("Mersedes");
+
+            student1.banckAcounts.bog = true;
+            student1.banckAcounts.liberti = true;
+            student1.banckAcounts.tbc = true;
+            ///Task 1
+            Console.WriteLine( GetHostingCountry("google.com"));
+            Console.ReadKey();
+        }
+
+
+
+        ///Task 1
+        static String GetHostingCountry(string URL)
+        {
+            if (URL.Contains("ge"))
+            {
+                return "GEORGIA";
+            }else if(URL.Contains("us")){
+                return "USA";
+            }
+            else if (URL.Contains("fr"))
+            {
+                return "France";
+            }
+            else if (URL.Contains("tr"))
+            {
+                return "Turkey";
+            }
+            else if (URL.Contains("pl"))
+            {
+                return "Poland ";
+            }
+            else if (URL.Contains("com"))
+            {
+                return "GLOBAL ";
+            }
+            else
+            {
+                return "Not detected!";
+            }
+        }
+
+        // TASK 3  Calculator
+        public static void calculator()
+        {
+
+            try
+            {   Console.WriteLine("input for calculation");
+            string textForCalculation = Console.ReadLine();
+            textForCalculation = textForCalculation.Replace(" ", String.Empty);
+
+            string value = new DataTable().Compute(textForCalculation, null).ToString();
+            Console.WriteLine(value);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR");
+            }
+         
+        }
+
+    }
+}
---
+++ I_Chkhitunidze_MidTerm/I_Chkhitunidze_MidTerm/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Chkhitunidze_MidTerm
+{
+    class Student
+    {
+        public string firsName { get; set; }
+        public string lastName { get; set; }
+        public string PN { get; set; }
+        public string address { get; set; }
+        public int groupID { get; set; }
+        public string mob { get; set; }
+        public DateTime registerDate = DateTime.Now;
+        public List<string> cars = new List<string>();
+        public BanckAcounts banckAcounts = new BanckAcounts();
+
+
+    }
+
+    partial class BanckAcounts
+    {
+        public bool liberti { get; set; }
+        public bool tbc { get; set; }
+        public bool bog { get; set; }
+        public bool VTB { get; set; }
+
+    }
+}
